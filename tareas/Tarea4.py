4a02f26495d23fccc74e6521b4456bd3a56458b0
AristidesOrtega <aog1983@gmail.com>
2016-02-14 21:51:38 -0500

    Delete Tarea4.py

--- tareas/Tarea4.py
+++
@@ -1,9 +0,0 @@
-#Dado un intervalo de tiempo en minutos, calcular los 
-#días, horas y minutos correspondientes.
-#Aristides Ortega
-tiempo = int(input("Introduce la cantidad de minutos "))
-dias = tiempo/1440
-x = tiempo %1440
-horas =x/60
-minutos= x%60
-print ("{0} dias, {1} minutos, {2} horas".format(dias,minutos,horas))
